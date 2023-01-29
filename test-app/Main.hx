ffb27d09d9fabe8a94db446b762ba6466f7cd294
jf <22012032+jobf@users.noreply.github.com>
2023-01-29 21:37:07 +0000

    tidy up scene init

--- test-app/Main.hx
+++ test-app/Main.hx
@@ -43,7 +43,9 @@ class Main extends Application {
 				throw("Sorry, only works with OpenGL.");
 		}
 	}
-	var init_layer :GraphicsConstructor;
+
+	var init_layer:GraphicsConstructor;
+
 	public function init(window:Window) {
 		var viewport_window:RectangleGeometry = {
 			y: 0,
@@ -53,73 +55,71 @@ class Main extends Application {
 		}
 
 		peoteview = new PeoteView(window);
-		// display_main = new Display(0, 0, 800, 640);
-		// peoteview.addDisplay(display_main);
-
-		// display_hud = new Display(0, 0, window.width, window.height);
-		// peoteview.addDisplay(display_hud);
-
-		// implementation_graphics = new Graphics(display_main, viewport_window);
 		implementation_input = new Input(window);
-		// implementation_graphics.set_color(Theme.bg_scene);
-		
-		
+
+		init_layer = () -> {
+			var display = new Display(0, 0, 800, 640);
+			peoteview.addDisplay(display);
+			return new Graphics(display, viewport_window, init_layer);
+		}
+
 		var storage = new Storage(window);
 		var file_list = storage.file_paths();
-		
-		if(file_list.length <= 0){
+
+		if (file_list.length <= 0) {
 			var file_name = '${Date.now().to_time_stamp()}.json';
-			
+
 			storage.file_save({
 				name: file_name,
 				content: Serialize.to_string({
 					models: []
 				})
 			});
-			
+
 			file_list = storage.file_paths();
-		}
-		else{
+		} else {
 			trace(file_list[0]);
 		}
-		
-		var init_scene:Game->Scene;
 
-		if(file_list.length > 0){
+		var start:SceneStart = DESIGN;
+
+		#if testoverview
+		start = OVERVIEW;
+		#end
+
+		#if testui
+		start = TESTUI;
+		#end
+
+		#if testfiles
+		start = STORAGE;
+		#end
+
+		if (file_list.length > 0) {
 			var index_end_of_list = file_list.length - 1;
 			var file_name = file_list[index_end_of_list];
 			var file_latest = storage.file_load(file_name);
 			// trace(file_latest.content);
 			var has_valid_file = file_latest != null && file_latest.content.length > 0;
-			if(has_valid_file){
+			if (has_valid_file) {
 				var file = Deserialize.parse_file_contents(file_latest.content);
-				init_scene = game -> new DesignerScene(game, viewport_window, Theme.bg_scene, file, file_name);
 
-				#if testoverview
-				init_scene = game -> new Overview(game, viewport_window, Theme.bg_scene, file);
-				#end
+				var init_scene:Game->Scene = switch start {
+					case DESIGN: game -> new DesignerScene(game, viewport_window, Theme.bg_scene, file, file_name);
+					case STORAGE: game -> new FileStorageScene(game, viewport_window, Theme.bg_scene);
+					case OVERVIEW: game -> new Overview(game, viewport_window, Theme.bg_scene, file);
+					case TESTUI: game -> new TestUi(game, viewport_window, Theme.bg_scene);
+				};
+
+				var init_scene_loader:Game->Scene = game -> new LoadingScene(preloader, init_scene, game, viewport_window, Theme.bg_scene);
+
+				game = new Game(init_scene_loader, init_layer, implementation_input, storage);
+				isReady = true;
+			}
+			else{
+				trace('no valid file to load ');
 			}
 		}
-		
-		#if testui
-		init_scene = game -> new TestUi(game, viewport_window, Theme.bg_scene);
-		#end
-
-		#if testfiles
-		init_scene = game -> new FileStorageScene(game, viewport_window, Theme.bg_scene);
-		#end
-
-		var init_scene_loader:Game->Scene = game -> new LoadingScene(preloader, init_scene, game, viewport_window, Theme.bg_scene);
-
-		init_layer  = () -> {
-			var display = new Display(0, 0, 800, 640);
-			peoteview.addDisplay(display);
-			return new Graphics(display, viewport_window, init_layer);
-		}
-
-		game = new Game(init_scene_loader, init_layer, implementation_input, storage);
-
-		isReady = true;
 	}
 
 	override function update(deltaTime:Int):Void {
@@ -145,5 +145,9 @@ class Main extends Application {
 	}
 }
 
-
-
+enum SceneStart {
+	DESIGN;
+	STORAGE;
+	OVERVIEW;
+	TESTUI;
+}
